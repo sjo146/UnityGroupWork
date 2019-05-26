4847b753749cdade7aef27ff5da1b81589d40120
铿然 <thd0106@163.com>
2019-05-26 13:47:01 +0800

    增加了开始页面以及加入了背景音乐
    增加了开始页面以及加入了背景音乐

---
+++ Assets/Script/StartButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    public Button start_btn;
+    public Button restart_btn;
+    public Button end_btn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        start_btn.onClick.AddListener(PlayGame);
+        restart_btn.onClick.AddListener(RePlayGame);
+        end_btn.onClick.AddListener(ExitGame);
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void RePlayGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
