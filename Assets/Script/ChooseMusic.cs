e2973c49de01309106da98469d46b51b5e6cccc1
铿然 <thd0106@163.com>
2019-05-27 19:09:32 +0800

    添加了多首背景音乐可供选择

---
+++ Assets/Script/ChooseMusic.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChooseMusic : MonoBehaviour
+{
+    public Dropdown dropdown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Dropdown.OptionData music1 = new Dropdown.OptionData();
+        music1.text = "双星物语";
+        Dropdown.OptionData music2 = new Dropdown.OptionData();
+        music2.text = "现在放下那把剑";
+        Dropdown.OptionData music3 = new Dropdown.OptionData();
+        music3.text = "Flower dance";
+        Dropdown.OptionData music4 = new Dropdown.OptionData();
+        music4.text = "风居住的街道";
+        Dropdown.OptionData music5 = new Dropdown.OptionData();
+        music5.text = "Luv Letter";
+        Dropdown.OptionData music6 = new Dropdown.OptionData();
+        music6.text = "遥远岁月的回忆";
+        dropdown.GetComponent<Dropdown>();
+        dropdown.options.Add(music1);
+        dropdown.options.Add(music2);
+        dropdown.options.Add(music3);
+        dropdown.options.Add(music4);
+        dropdown.options.Add(music5);
+        dropdown.options.Add(music6);
+
+    }
+    public void SelectMusic()
+    {
+        Debug.Log(transform.GetComponent<Dropdown>().value);
+        GlobalMusic._instance.changeMusic(transform.GetComponent<Dropdown>().value);
+    }
+
+}
--- Assets/Script/GlobalMusic.cs
+++ Assets/Script/GlobalMusic.cs
@@ -5,12 +5,22 @@ using UnityEngine.UI;
 
 public class GlobalMusic : MonoBehaviour
 {
-    public AudioClip music;
+    public static GlobalMusic _instance;
+    public AudioClip music1;
+    public AudioClip music2;
+    public AudioClip music3;
+    public AudioClip music4;
+    public AudioClip music5;
+    public AudioClip music6;
     private AudioSource back;
     public GameObject globalMusic;
     GameObject MyMusic;
     public Slider slider;
     // Start is called before the first frame update
+    private void Awake()
+    {
+        _instance = this;
+    }
     void Start()
     {
         MyMusic = GameObject.FindGameObjectWithTag("GlobalUIMusic");
@@ -21,7 +31,7 @@ public class GlobalMusic : MonoBehaviour
         back = MyMusic.GetComponent<AudioSource>();
         back.loop = true; //设置循环播放  
         back.volume = 0.3f;//设置音量最大，区间在0-1之间
-        back.clip = music;
+        back.clip = music1;
         back.Play(); //播放背景音乐，
         slider.value = 0.5f;
     }
@@ -30,5 +40,18 @@ public class GlobalMusic : MonoBehaviour
     void Update()
     {
         back.volume = slider.value;
+    }
+
+    public void changeMusic(int i)
+    {
+        if (i == 0) back.clip = music1;
+        else if(i==1) back.clip = music2;
+        else if(i==2) back.clip = music3;
+        else if (i == 3) back.clip = music4;
+        else if (i == 4) back.clip = music5;
+        else if (i == 5) back.clip = music6;
+        back.Play();
+        back.volume = slider.value;
+
     }
 }
